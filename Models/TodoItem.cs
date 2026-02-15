781b62ab44ae7d58c499c914c28e221beffb43c1
IcyAuk <r.nvessah@gmail.com>
2026-02-15 17:55:54 +0100

    feat: added dbcontext

--- Models/TodoItem.cs
+++ Models/TodoItem.cs
@@ -2,7 +2,7 @@ namespace TodoApi.Models;
 
 public class TodoItem
 {
-    public long Id {get;set;}
+    public long Id {get;set;} //property Id is automatically treated like PRIMARY KEY
     public string? Name {get;set;}
     public bool IsComplete {get;set;}
 }
